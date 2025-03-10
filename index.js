97244b18a56b2c6d6d486df6a9153a302a63c56d
paigemeyer <paigemeyer71@icloud.com>
2025-03-10 13:24:57 +0200

    added new JS elements
    added an api

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 function searchNow(event){
 event.preventDefault();
-let newSearch=document.querySelector("#new-city-input");
+let SearchInput=document.querySelector("#new-city-input");
 let cityElement=document.querySelector("#heading-city");
-cityElement.innerHTML=newSearch.value;
+cityElement.innerHTML=SearchInput.value;
+searchCity(SearchInput.value);
+
 }
 let serachEngine=document.querySelector("#search-input");
 serachEngine.addEventListener("submit", searchNow);
+
+
+
+function searchCity(city){
+let apiKey="f333f4tb5038acab6309b2fo8d0b9912"
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+axios.get(apiUrl).then(updateTemp);}
+
+function updateTemp(response){
+let temperature=document.querySelector("#temp");
+let tempElement=response.data.temperature.current;
+temperature.innerHTML=Math.round(tempElement);
+}
+
